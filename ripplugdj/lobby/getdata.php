d11324e6661bd18bd060ede1dafef6bde818cef5
ladaliska <76939998+ladaliska@users.noreply.github.com>
2023-03-26 16:54:22 +0200

    Delete ripplugdj directory

--- ripplugdj/lobby/getdata.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$url = "http://www.youtube.com/watch?v=C4kxS1ksqtw&feature=relate";
-parse_str( parse_url( $url, PHP_URL_QUERY ), $my_array_of_vars );
-echo $my_array_of_vars['v'];
-
-?>
--- ripplugdj/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-$password = "123456";
-$hashed_password ='QiP3HsIY92nytXdIx4uEm.UCi/3YXwy2BlksPf';
-var_dump(password_verify($password, $hashed_password));
-?>
